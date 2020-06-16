196bf1c1cb6b0d3992b19fd40491b7a031bf6abd
root <root@DESKTOP-PG69SAM.localdomain>
2020-06-17 00:29:41 +0800

    server:cors&web:frame init

--- blog-test/index.js
+++ blog-test/index.js
@@ -1,9 +1,11 @@
-import Koa from 'koa'
+import Koa from "koa"
 const app = new Koa()
+
+import cors from "koa2-cors"
 // 静态服务中间件
-import serve from 'koa-static'
+import serve from "koa-static"
 // 路由
-import router from './middleware/router'
+import router from "./middleware/router"
 // 参数解析中间件
 // import bodyParser from 'koa-bodyparser'
 // // 模板引擎中间件
@@ -13,13 +15,21 @@ import router from './middleware/router'
 // 数据库中间件
 // import db from './middleware/db'
 // app.use(views(`${__dirname}/static/html`, { extension: 'ejs' }))
-
+app.use(
+  cors({
+    origin: (ctx) => {
+      return ctx.request.header.origin
+    },
+    credentials: true,
+  })
+)
 app.use(router.routes())
 
 // app.use(upload)
 
-app.use(serve(`${__dirname}/static/html`, { extensions: ['html'] }))
+app.use(serve(`${__dirname}/static/html`, { extensions: ["html"] }))
+app.use(serve(`${__dirname}/static/blogs`, { extensions: ["txt"] }))
 
 app.listen(8765, () => {
-    console.log('Listening on 8765')
+  console.log("Listening on 8765")
 })
